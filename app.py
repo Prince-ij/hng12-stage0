b2d5253b218d618bce4f56da0d9a86407bd859f7
root <root@LAPTOP-56RVIP6N.localdomain>
2025-01-29 07:38:54 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from datetime import datetime
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def home():
+    response = {
+        "email": "aminuibrahimthefifth@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "github_url": "https://github.com/Prince-ij/hng12-stage0"
+    }
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
